1ebbc72d8be904ac11076a82ca33a3d1dd342028
NikonD <nikon9nikon@gmail.com>
2023-08-06 19:58:10 +0600

    ip

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -5,25 +5,25 @@ import AuthForm from './components/AuthForm';
 interface AuthContainerProps {
   open: boolean,
   setOpen: any,
-  user:any
+  user: any
 }
 
-const AuthContainer: React.FC<AuthContainerProps> = ({open, setOpen, user}) => {
+const AuthContainer: React.FC<AuthContainerProps> = ({ open, setOpen, user }) => {
   // const [open, setOpen] = useState(false)
   const handleGoogleAuth = () => {
     console.log('Google Auth');
   };
 
   return (
-    
-    <div style={{zIndex: 1000}} className={`absolute auth-container bg-white shadow-md fixed top-0 bottom-0 ${open ? 'open' : ''}`}>
-      
-      <button onClick={()=>{setOpen(open? false: true)}} className="bg-gray-300 text-white rounded-md py-2 px-4">
-        {open  ? 'Close' : 'Open'}
+
+    <div style={{ zIndex: 1000 }} className={`absolute auth-container bg-white shadow-md fixed top-0 bottom-0 ${open ? 'open' : ''} ${open ? '' : 'hidden'}`}>
+
+      <button onClick={() => { setOpen(open ? false : true) }} className="bg-gray-300 text-white rounded-md py-2 px-4">
+        {open ? 'Close' : 'Open'}
       </button>
       <div className="p-6">
-        {user? user.username : <AuthForm onGoogleAuth={handleGoogleAuth} />}
-        
+        {user ? user.username : <AuthForm onGoogleAuth={handleGoogleAuth} />}
+
       </div>
     </div>
   );
